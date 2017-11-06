f958b9d1fc79640e7222c1bde4bbe17c3278a3c4
adityachamp <31718627+adityachamp@users.noreply.github.com>
2017-11-06 12:18:40 -0500

    Update ColorImage.cpp

--- ColorImage.cpp
+++ ColorImage.cpp
@@ -1,3 +1,4 @@
+
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -13,34 +14,40 @@ int main(int argc, char** argv )
     Mat src;
     src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     namedWindow( "Original image", CV_WINDOW_AUTOSIZE );
-    imshow( "Original image", src);
+    imshow( "Original image", src); // THIS IS DISPLAYING THE ORIGINAL COLOURED IMAGE OF THE LADY.
 
 	vector<Mat> input_planes(3);
 	split(src,input_planes);
 	Mat channel1_display, channel2_display, channel3_display;
-        imshow("Red",   input_planes[2]);
-        imshow("Green",   input_planes[1]);
-        imshow("Blue",   input_planes[0]);
+	// IT IS USING BGR FORMAT HERE(3 CHANNELS)
+	// HERE ALL THE 3 CHANNELS ARE BEING DISPLAYED INDIVIDUALLY
+        imshow("Red",   input_planes[2]); //RED CHANNEL
+        imshow("Green",   input_planes[1]); // GREEN CHANNEL
+        imshow("Blue",   input_planes[0]); // BLUE CHANNEL
 
 
 	Mat ycrcb_image;
-	cvtColor(src, ycrcb_image, CV_BGR2YCrCb);
+	// THIS IS THE YCbCr FORMAT WHICH ALSO HAS 3 CHANNELS , Y - LUMA COMPONENT, Cb - BLUE DIFFERENCE , Cr - RED DIFFERENCE 
+	// AGAIN THE INDIVIDUAL CHANNELS ARE DISPALYED
+	cvtColor(src, ycrcb_image, CV_BGR2YCrCb); // THIS CONVERTS THE IMAGE TO YCrCb format
 	split(ycrcb_image,input_planes);
-        imshow("Y",   input_planes[0]);
-        imshow("Cb",   input_planes[1]);
-        imshow("Cr",   input_planes[2]);
+        imshow("Y",   input_planes[0]); // Y CHANNEL
+        imshow("Cb",   input_planes[1]); // Cb CHANNEL
+        imshow("Cr",   input_planes[2]); // Cr CHANNEL
 
 
 	Mat hsv_image;
-	cvtColor(src, hsv_image, CV_BGR2HSV);
+	cvtColor(src, hsv_image, CV_BGR2HSV);// THIS CONVERTS THE IMAGE TO HSV format
 	vector<Mat> hsv_planes(3);
+	// HSV - HUE , SATURATION , VALUE
 	split(hsv_image,hsv_planes);
-        imshow("Hue",   hsv_planes[0]);
-        imshow("Saturation",   hsv_planes[1]);
-        imshow("Value",   hsv_planes[2]);
+        imshow("Hue",   hsv_planes[0]); // H CHANNEL
+        imshow("Saturation",   hsv_planes[1]); // SATURATION  CHANNEL
+        imshow("Value",   hsv_planes[2]); // VALUE CHANNEL 
 
 
 	
-    waitKey(0);                                       
+    waitKey(0); // THIS SUGGESTS THAT THE IMAGE SHOULD BE VISIBLE TILL A USER DOESNT CLICK ANY KEY.                                      
     return 0;
 } 
+
